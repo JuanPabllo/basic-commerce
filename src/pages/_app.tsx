8253837eb2f892482705a5b41790b7e70be2b2a8
Emmanoel Coutinho <emmanoelcesar967@gmail.com>
2021-03-15 15:45:22 -0300

    dependecies configuration

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import App from 'next/app';
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+    return <Component {...pageProps} />
+  }
+}
+
