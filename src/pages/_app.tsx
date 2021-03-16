ef5c84fd8bfa300382e4bb944af0c466b668af80
Juan Pablo <59495901+JuanPabllo@users.noreply.github.com>
2021-03-16 19:29:21 -0300

    add types of styled compenents and styles light

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
-import App from 'next/app';
-import GlobalStyles from '../styles/GlobalStyles';
-import '../globals.css';
+import App from "next/app";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../styles/GlobalStyles";
+import "../globals.css";
+
+import { light } from "../styles/theme/light";
 
 export default class MyApp extends App {
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps } = this.props;
     return (
-      <>
+      <ThemeProvider theme={light}>
         <GlobalStyles />
         <Component {...pageProps} />
-      </>
+      </ThemeProvider>
     );
   }
 }
-
