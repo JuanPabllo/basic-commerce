676ab092573ce7cc5f73e50a464f320971c7979d
Juan Pablo <59495901+JuanPabllo@users.noreply.github.com>
2021-03-16 19:29:51 -0300

    add types of styled compenents and styles light

---
+++ src/styles/theme/light.ts
@@ -0,0 +1,17 @@
+export const light = {
+  title: "light",
+
+  colors: {
+    primary: "#FF6C00",
+    secondary: "#EBEBEB",
+    tertiary: "#c3d8",
+
+    black: "#000",
+    white: "#FFFFFF",
+    gray: "#BFBFBF",
+
+    success: "#03BB85",
+    hover: "#D45A00",
+    warning: "#E44C4E",
+  },
+};
