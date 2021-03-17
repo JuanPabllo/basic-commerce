366385036eac8d29f31fb10dd7f7f7185cd00a94
EmmanoelCoutinho <emmanoelcesar967@gmail.com>
2021-03-17 15:10:48 -0300

    eslint adjusts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,46 +2,46 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <title>Commerce</title>
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Commerce</title>
 
-                    <link rel="shortcut icon" href="favicon.jpg" type="image/png" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
+          <link rel="shortcut icon" href="favicon.jpg" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
     }
-
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    static async getInitialProps(ctx) {
-        const sheet = new ServerStyleSheet();
-        const originalRenderPage = ctx.renderPage;
-
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
-                });
-
-            const initialProps = await Document.getInitialProps(ctx);
-            return {
-                ...initialProps,
-                styles: (
-                    <>
-                        {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                )
-            };
-        } finally {
-            sheet.seal();
-        }
-    }
+  }
 }
