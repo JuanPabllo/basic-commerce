34965efed78f78f9264d9333b173ab8c47694b67
EmmanoelCoutinho <emmanoelcesar967@gmail.com>
2021-03-20 19:18:18 -0300

    some adjuste part 2

---
+++ src/components/Prices/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export const Prices: React.FC = () => {
+  return (
+    <Container>
+      <h1>Hello!</h1>
+    </Container>
+  );
+};
---
+++ src/components/Title/index.tsx
@@ -0,0 +1,5 @@
+import { Strong } from './style';
+
+export const Title: React.FC = ({ children }) => {
+  return <Strong>{children}</Strong>;
+};
