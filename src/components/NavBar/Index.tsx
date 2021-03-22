d82e046c61bdf23c3008934e78f5d1d3931f54cc
JuanPabllo <juanpablo192.dreaw@gmail.com>
2021-03-22 15:13:05 -0300

    Changing navbar colors

--- src/components/NavBar/Index.tsx
+++ src/components/NavBar/Index.tsx
@@ -1,13 +1,21 @@
+import { useRouter } from 'next/router';
+
 import { ButtonNav, ButtonNavActived, MainNav, TextContainer } from './Styles';
 
 const NavBar: React.FC = () => {
+  const router = useRouter();
+  const path = router.pathname;
   return (
     <MainNav>
       <TextContainer>
-        <ButtonNavActived>SACOLA</ButtonNavActived>
+        {path === '/' ? <ButtonNavActived>SACOLA</ButtonNavActived> : <ButtonNav>SACOLA</ButtonNav>}
       </TextContainer>
       <TextContainer>
-        <ButtonNav>PAGAMENTO</ButtonNav>
+        {path === '/formPayments' ? (
+          <ButtonNavActived>PAGAMENTO</ButtonNavActived>
+        ) : (
+          <ButtonNav>PAGAMENTO</ButtonNav>
+        )}
       </TextContainer>
       <TextContainer>
         <ButtonNav>CONFIRMAÇÃO</ButtonNav>
