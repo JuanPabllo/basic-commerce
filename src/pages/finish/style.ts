d027cca8498b87c62c8e1f4aeabdf15376273a0c
EmmanoelCoutinho <emmanoelcesar967@gmail.com>
2021-03-25 17:53:25 -0300

    add icon on finish

--- src/pages/finish/style.ts
+++ src/pages/finish/style.ts
@@ -11,8 +11,29 @@ export const Container = styled.div`
   background: ${(props) => props.theme.colors.secondary};
 `;
 
+export const IconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 1rem auto 0;
+  width: 95%;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const IconText = styled.h1`
+  color: ${(props) => props.theme.colors.primary};
+`;
+
 export const FlexTitle = styled.div`
   margin: 12px 1rem 5px;
+
+  @media (min-width: 720px) {
+    margin: 12px 3rem 5px;
+  }
+  @media (min-width: 1250px) {
+    margin: 12px 3.5rem 5px;
+  }
 `;
 
 export const PaymentsContainer = styled.div`
